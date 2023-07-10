460f41484c088c04ba913f132063f45bc0f0147d
Angela <angelakulola4@gmail.com>
2023-07-10 15:18:02 +0300

    Completed Assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    case
+    when (side1 > 0 && side2 > 0 && side3 > 0) && ((side1 + side2 > side3) && (side2 + side3 > side1) && (side3 + side1 > side2))
+      case
+      when @side1 == @side2 && @side2 == @side3
+        :equilateral
+      when @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      puts "This is an invalid triangle"
+    end
+  end
 end
