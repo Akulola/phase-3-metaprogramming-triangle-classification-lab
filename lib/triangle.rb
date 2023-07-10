fe01d6a50ade47337e3bfff23743d24f9ee4202f
Angela <angelakulola4@gmail.com>
2023-07-10 15:20:31 +0300

    Completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,12 +9,15 @@ class Triangle
   end
 
   def kind
+    sides_positive = side1 > 0 && side2 > 0 && side3 > 0
+    triangle_inequality = (side1 + side2 > side3) && (side2 + side3 > side1) && (side3 + side1 > side2)
+  
     case
-    when (side1 > 0 && side2 > 0 && side3 > 0) && ((side1 + side2 > side3) && (side2 + side3 > side1) && (side3 + side1 > side2))
+    when sides_positive && triangle_inequality
       case
-      when @side1 == @side2 && @side2 == @side3
+      when side1 == side2 && side2 == side3
         :equilateral
-      when @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+      when side1 == side2 || side2 == side3 || side1 == side3
         :isosceles
       else
         :scalene
@@ -23,6 +26,7 @@ class Triangle
       raise TriangleError
     end
   end
+  
 
   class TriangleError < StandardError
     def message
